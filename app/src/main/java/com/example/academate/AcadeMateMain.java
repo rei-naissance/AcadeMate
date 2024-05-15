5adc5bd22d5146aad10c470574213d6a5d512397
rei <philippeandreid@gmail.com>
2024-05-15 19:15:40 +0800

    func: project initialization
    Co-authored-by: Arcelyn Tequillo <arcelyn.tequillo0220@gmail.com>

---
+++ app/src/main/java/com/example/academate/AcadeMateMain.java
@@ -0,0 +1,14 @@
+package com.example.academate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcadeMateMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.academate_main);
+    }
+}
