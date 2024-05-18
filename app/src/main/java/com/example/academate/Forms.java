e22d935277a1ee9144752d2d19250dbce0c5b6d1
Brawler612 <brawler612@gmail.com>
2024-05-19 02:34:54 +0800

    Academate_forms.xml almost complete!

---
+++ app/src/main/java/com/example/academate/Forms.java
@@ -0,0 +1,14 @@
+package com.example.academate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.academate_forms);
+    }
+}
