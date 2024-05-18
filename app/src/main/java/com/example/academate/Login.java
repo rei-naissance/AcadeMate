e9cdf3097eaad1eb823f0c1b42eeecd3265be616
arce <arcelyn.tequillo0220@gmail.com>
2024-05-18 19:26:53 +0800

    feat: base designs

---
+++ app/src/main/java/com/example/academate/Login.java
@@ -0,0 +1,32 @@
+package com.example.academate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+   Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.academate_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainScreen.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/academate/MainScreen.java
@@ -0,0 +1,45 @@
+package com.example.academate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.academate.databinding.AcademateHomeBinding;
+import com.example.academate.databinding.AcademateMainScreenBinding;
+
+public class MainScreen extends AppCompatActivity {
+    AcademateMainScreenBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = AcademateMainScreenBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        replaceFragment(new Home());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new Home());
+            } else if (itemId == R.id.add) {
+                replaceFragment(new Add());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new Profile());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+}
