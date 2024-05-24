97aaea52b4ea5067b1b1fa6eac4a068acf2b1e54
arce <arcelyn.tequillo0220@gmail.com>
2024-05-24 21:04:08 +0800

    feat: layout design updated.

--- app/src/main/java/com/example/academate/Home.java
+++ app/src/main/java/com/example/academate/Home.java
@@ -7,21 +7,37 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.academate.databinding.AcademateHomeBinding;
 import com.example.academate.databinding.AcademateMainBinding;
 
 public class Home extends Fragment {
 
+    ImageButton btnLaptop;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.academate_home, container, false);
+        View view = inflater.inflate(R.layout.academate_home, container, false);
+        btnLaptop = (ImageButton) view.findViewById(R.id.btnLaptop);
+
+        btnLaptop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ItemDetails.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+
+
     }
 
 }
