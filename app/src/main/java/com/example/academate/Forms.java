2e6899d4042b7b472b6c2b2b01a7561b6d5b812c
rei <philippeandreid@gmail.com>
2024-05-26 01:51:34 +0800

    feat: working signup and login

--- app/src/main/java/com/example/academate/Forms.java
+++ app/src/main/java/com/example/academate/Forms.java
@@ -9,6 +9,6 @@ public class Forms extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.academate_forms);
+//        setContentView(R.layout.academate_forms);
     }
 }
