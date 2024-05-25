29027f36d2a093182d26031120385111306a86fe
arce <arcelyn.tequillo0220@gmail.com>
2024-05-26 03:26:28 +0800

    feat: add screen added

--- app/src/main/java/com/example/academate/ItemDetails.java
+++ app/src/main/java/com/example/academate/ItemDetails.java
@@ -2,13 +2,26 @@ package com.example.academate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class  ItemDetails extends AppCompatActivity {
-
+    Button borrowItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.academate_itemdetails);
+
+        borrowItem = findViewById(R.id.btnBorrowItem);
+
+        borrowItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ItemDetails.this, Forms.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/academate/Signup.java
+++ app/src/main/java/com/example/academate/Signup.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
 
     TextView login;
+    Button btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +25,6 @@ public class Signup extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
